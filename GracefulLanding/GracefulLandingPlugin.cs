d1457eccfeeab2a4d39637c9b677cfb8776ccaba
Crystal <Crystal>
2021-03-21 04:46:06 -0400

    Created GracefulLanding mod

---
+++ GracefulLanding/GracefulLandingPlugin.cs
@@ -0,0 +1,81 @@
+﻿// Copyright 2021 Crystal Ferrai
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GracefulLanding
+{
+    [BepInPlugin("dev.crystal.gracefullanding", "Graceful Landing", "1.0.0.0")]
+    [BepInProcess("valheim.exe")]
+    [BepInProcess("valheim_server.exe")]
+    public class GracefulLandingPlugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<float> MinDamageHeight;
+        public static ConfigEntry<float> MaxDamageHeight;
+        public static ConfigEntry<float> MaxDamageAmount;
+
+        private void Awake()
+        {
+            MinDamageHeight = Config.Bind("Falling", nameof(MinDamageHeight), 8.0f, "The minimum distance you must fall to receive any fall damage. Allowed range 1-10000. Game default 4.");
+            if (MinDamageHeight.Value < 1.0f) MinDamageHeight.Value = 1.0f;
+            if (MinDamageHeight.Value > 10000.0f) MinDamageHeight.Value = 10000.0f;
+
+            MaxDamageHeight = Config.Bind("Falling", nameof(MaxDamageHeight), 64.0f, $"The minimum distance you must to receive maximum fall damage. Allowed range 1-10000. Must be equal to or higher than {nameof(MinDamageHeight)}. Game default 16.");
+            if (MaxDamageHeight.Value < MinDamageHeight.Value) MaxDamageHeight.Value = MinDamageHeight.Value;
+            if (MaxDamageHeight.Value > 10000.0f) MaxDamageHeight.Value = 10000.0f;
+
+            MaxDamageAmount = Config.Bind("Falling", nameof(MaxDamageAmount), 100.0f, "The maximum fall damage that can be received. Allowed range 0-10000. Game default 100.");
+            if (MaxDamageAmount.Value < 0.0f) MaxDamageAmount.Value = 0.0f;
+            if (MaxDamageAmount.Value > 10000.0f) MaxDamageAmount.Value = 10000.0f;
+
+            Harmony.CreateAndPatchAll(typeof(Character_UpdateGroundContact_Patch));
+        }
+
+        [HarmonyPatch(typeof(Character), "UpdateGroundContact")]
+        private static class Character_UpdateGroundContact_Patch
+        {
+            private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                foreach (CodeInstruction instruction in instructions)
+                {
+                    if (instruction.opcode == OpCodes.Ldc_R4)
+                    {
+                        switch ((float)instruction.operand)
+                        {
+                            case 4.0f:
+                                instruction.operand = MinDamageHeight.Value;
+                                break;
+                            case 16.0f:
+                                instruction.operand = MaxDamageHeight.Value;
+                                break;
+                            case 100.0f:
+                                instruction.operand = MaxDamageAmount.Value;
+                                break;
+                        }
+                    }
+                    yield return instruction;
+                }
+            }
+        }
+    }
+}
---
+++ GracefulLanding/package/README.md
@@ -0,0 +1,18 @@
+﻿Reduces fall damage.
+
+This is a very simple mod that replaces a few fall damage related values with configurable values. By default, you can fall twice as far before taking any damage and four times as far before taking max damage.
+
+Run the game once with the mod enabled to generate the config. See config for details on what each option does.
+
+## Installation
+This mod is designed to install and run via [r2modman](https://thunderstore.io/package/ebkr/r2modman/). You can optionally install it manually following the steps below.
+
+**Manual Install**
+1. Install [BepInExPack Valheim](https://valheim.thunderstore.io/package/denikson/BepInExPack_Valheim/)
+2. Download latest ``GracefulLanding.dll`` by clicking "Manual Download". Extract the dll from the zip file to into ``[GameDirectory]\Bepinex\plugins``. (You only need the dll.)
+3. Run the game once, then close it and edit the generated cfg file in ``[GameDirectory]\Bepinex\config`` if you want to customize anything.
+
+## Changelog
+1.0.0
+
+* Initial release
